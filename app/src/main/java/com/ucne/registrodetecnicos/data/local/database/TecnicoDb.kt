0ca3622092ab1be947935b63bd63b690b704208e
ALEXANDER SUAREZ TAVERAS <ALEXANDER_SUAREZ@ucne.edu.do>
2024-05-09 16:26:04 -0400

    Agregando TecnicoDb

---
+++ app/src/main/java/com/ucne/registrodetecnicos/data/local/database/TecnicoDb.kt
@@ -0,0 +1,17 @@
+package com.ucne.registrodetecnicos.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.ucne.registrodetecnicos.data.local.dao.TecnicoDao
+import com.ucne.registrodetecnicos.data.local.entities.TecnicoEntity
+
+@Database(
+    entities = [
+        TecnicoEntity::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+abstract class TecnicoDb: RoomDatabase() {
+    abstract fun tecnicoDao(): TecnicoDao
+}
